353ace0ab09bed238416b163ae90b59f47f8f2d0
qianlAIyf <1769234376@qq.com>
2020-04-06 22:45:36 +0800

    .

--- src/StringTest.java
+++ src/StringTest.java
@@ -1,7 +1,22 @@
 
 public class StringTest {
+	/**
+	 * @param args
+	 */
 	public static void main(String [] args) {
 		String string = "hello,world!";
-		System.out.print(string.substring(0,5) + " " + string.substring(6));
+		System.out.println("delete: " + string.substring(0,5) + " " + string.substring(6));	//delete character(s) from string
+		
+		System.out.println("replace: " + string.replace('h', 'H'));//replace character(s)
+		
+		System.out.println("reverse: " + new StringBuffer(string).reverse().toString());
+		
+		//substring search, return position of the first letter in substring
+		Integer integer = string.indexOf(",");
+		if(integer != -1) {
+			System.out.println("index: " + integer);
+		}else {
+			System.out.println("not found!");
+		}
 	}
 }
